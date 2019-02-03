65143eb042d7db3b7dce93f77500ec2e3305958c
Alexey Kotlyarov <a@koterpillar.com>
2019-02-03 15:44:31 +1100

    Implement basic codecs

--- src/main/scala/com/koterpillar/json/AllInstances.scala
+++ src/main/scala/com/koterpillar/json/AllInstances.scala
@@ -1,6 +1,7 @@
 package com.koterpillar.json
-import io.circe.Json
+import io.circe._
 import io.circe.syntax._
+import monocle.Prism
 
 trait AllInstances {
   def simpleType(typeValue: String): Json = Json.obj("type" -> typeValue.asJson)
@@ -10,13 +11,21 @@ trait AllInstances {
 
   implicit val intSchema: Schema[Int] = new Schema[Int] {
     override def schema: Json = number
+
+    override def codec: Prism[Json, Int] = Prism[Json, Int] {
+      obj => obj.asNumber.flatMap(_.toInt)
+    }(Json.fromInt)
   }
 
   implicit val stringSchema: Schema[String] = new Schema[String] {
     override def schema: Json = string
+
+    override def codec: Prism[Json, String] = Prism[Json, String](_.asString)(Json.fromString)
   }
 
   implicit val booleanSchema: Schema[Boolean] = new Schema[Boolean] {
     override def schema: Json = boolean
+
+    override def codec: Prism[Json, Boolean] = Prism[Json, Boolean](_.asBoolean)(Json.fromBoolean)
   }
 }
