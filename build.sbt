afc27a3f7369237d72545ed12e58ca91a4ac5011
Alexey Kotlyarov <a@koterpillar.com>
2019-02-03 15:26:45 +1100

    Add codec member to Schema

--- build.sbt
+++ build.sbt
@@ -6,6 +6,7 @@ val disciplineVersion = "0.9.0"
 val circeVersion = "0.9.1"
 val catsVersion = "1.0.0"
 val monocleVersion = "1.5.0"
+val shapelessVersion = "2.3.3"
 
 lazy val lensJson = project
   .in(file("."))
@@ -23,5 +24,7 @@ lazy val lensJson = project
       "com.github.julien-truffaut" %%  "monocle-core"  % monocleVersion,
       "com.github.julien-truffaut" %%  "monocle-macro" % monocleVersion,
       "com.github.julien-truffaut" %%  "monocle-law"   % monocleVersion % Test,
+
+      "com.chuusai" %% "shapeless" % shapelessVersion,
     )
   )
--- src/main/scala/com/koterpillar/json/Object.scala
+++ src/main/scala/com/koterpillar/json/Object.scala
@@ -1,7 +1,9 @@
 package com.koterpillar.json
 
-import io.circe.Json
+import io.circe.{Json, JsonObject}
 import io.circe.syntax._
+import monocle.Prism
+import shapeless._
 
 trait Field_ {
   def name: String
@@ -18,12 +20,38 @@ sealed trait Object[A] extends Schema[A] {
     "fields" -> Json.obj((for { field <- fields} yield { field.name -> field.schema.schema }): _*),
     "required" -> fields.map(_.name).asJson,
   )
+
+  def objectCodec: Prism[JsonObject, A]
+
+  override def codec: Prism[Json, A] = Object.jsonObject.composePrism(objectCodec)
 }
 
-case object NullObject extends Object[Unit] {
+object Object {
+  def jsonObject: Prism[Json, JsonObject] = Prism((j: Json) => j.asObject)(Json.fromJsonObject)
+}
+
+case object NullObject extends Object[HNil] {
   override def fields: List[Field_] = Nil
+
+  override def objectCodec: Prism[JsonObject, HNil] = Prism[JsonObject, HNil] {
+    obj =>
+    if(obj.toMap == Map.empty) Some(HNil) else None
+  }(_ => JsonObject.empty)
 }
 
-final case class ConsObject[H, T](head: Field[H], tail: Object[T]) extends Object[(H, T)] {
+final case class ConsObject[H, T <: HList](head: Field[H], tail: Object[T]) extends Object[H :: T] {
   override def fields: List[Field_] = head :: tail.fields
+
+  override def objectCodec: Prism[JsonObject, H :: T] = {
+    def getOption(json: JsonObject): Option[H :: T] = for {
+      headJson <- json(head.name)
+      headValue <- head.schema.codec.getOption(headJson)
+      tailJson = json.remove(head.name)
+      tailValue <- tail.objectCodec.getOption(tailJson)
+    } yield headValue :: tailValue
+
+    def reverseGet(value: H :: T): JsonObject = ???
+
+    Prism(getOption)(reverseGet)
+  }
 }
