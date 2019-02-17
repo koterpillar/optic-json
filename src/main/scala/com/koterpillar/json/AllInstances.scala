ed33d16da3d9d97b67207e2a6741c8f1c124496c
Alexey Kotlyarov <a@koterpillar.com>
2019-02-17 16:25:43 +1100

    Scalafmt

--- src/main/scala/com/koterpillar/json/AllInstances.scala
+++ src/main/scala/com/koterpillar/json/AllInstances.scala
@@ -5,27 +5,30 @@ import monocle.Prism
 
 trait AllInstances {
   def simpleType(typeValue: String): Json = Json.obj("type" -> typeValue.asJson)
-  def number: Json = simpleType("number")
-  def string: Json = simpleType("string")
-  def boolean: Json = simpleType("boolean")
+  def number: Json                        = simpleType("number")
+  def string: Json                        = simpleType("string")
+  def boolean: Json                       = simpleType("boolean")
 
   implicit val intSchema: Schema[Int] = new Schema[Int] {
     override def schema: Json = number
 
-    override def codec: Prism[Json, Int] = Prism[Json, Int] {
-      obj => obj.asNumber.flatMap(_.toInt)
-    }(Json.fromInt)
+    override def codec: Prism[Json, Int] =
+      Prism[Json, Int] { obj =>
+        obj.asNumber.flatMap(_.toInt)
+      }(Json.fromInt)
   }
 
   implicit val stringSchema: Schema[String] = new Schema[String] {
     override def schema: Json = string
 
-    override def codec: Prism[Json, String] = Prism[Json, String](_.asString)(Json.fromString)
+    override def codec: Prism[Json, String] =
+      Prism[Json, String](_.asString)(Json.fromString)
   }
 
   implicit val booleanSchema: Schema[Boolean] = new Schema[Boolean] {
     override def schema: Json = boolean
 
-    override def codec: Prism[Json, Boolean] = Prism[Json, Boolean](_.asBoolean)(Json.fromBoolean)
+    override def codec: Prism[Json, Boolean] =
+      Prism[Json, Boolean](_.asBoolean)(Json.fromBoolean)
   }
 }
--- src/main/scala/com/koterpillar/json/Object.scala
+++ src/main/scala/com/koterpillar/json/Object.scala
@@ -17,42 +17,48 @@ sealed trait Object[A] extends Schema[A] {
 
   override def schema: Json = Json.obj(
     "type" -> "object".asJson,
-    "fields" -> Json.obj((for { field <- fields} yield { field.name -> field.schema.schema }): _*),
+    "fields" -> Json.obj((for { field <- fields } yield {
+      field.name -> field.schema.schema
+    }): _*),
     "required" -> fields.map(_.name).asJson,
   )
 
   def objectCodec: Prism[JsonObject, A]
 
-  override def codec: Prism[Json, A] = Object.jsonObject.composePrism(objectCodec)
+  override def codec: Prism[Json, A] =
+    Object.jsonObject.composePrism(objectCodec)
 }
 
 object Object {
-  def jsonObject: Prism[Json, JsonObject] = Prism((j: Json) => j.asObject)(Json.fromJsonObject)
+  def jsonObject: Prism[Json, JsonObject] =
+    Prism((j: Json) => j.asObject)(Json.fromJsonObject)
 }
 
 case object NullObject extends Object[HNil] {
   override def fields: List[Field_] = Nil
 
-  override def objectCodec: Prism[JsonObject, HNil] = Prism[JsonObject, HNil] {
-    obj =>
-    if(obj.toMap == Map.empty) Some(HNil) else None
-  }(_ => JsonObject.empty)
+  override def objectCodec: Prism[JsonObject, HNil] =
+    Prism[JsonObject, HNil] { obj =>
+      if (obj.toMap == Map.empty) Some(HNil) else None
+    }(_ => JsonObject.empty)
 }
 
-final case class ConsObject[H, T <: HList](head: Field[H], tail: Object[T]) extends Object[H :: T] {
+final case class ConsObject[H, T <: HList](head: Field[H], tail: Object[T])
+    extends Object[H :: T] {
   override def fields: List[Field_] = head :: tail.fields
 
   override def objectCodec: Prism[JsonObject, H :: T] = {
-    def getOption(json: JsonObject): Option[H :: T] = for {
-      headJson <- json(head.name)
-      headValue <- head.schema.codec.getOption(headJson)
-      tailJson = json.remove(head.name)
-      tailValue <- tail.objectCodec.getOption(tailJson)
-    } yield headValue :: tailValue
+    def getOption(json: JsonObject): Option[H :: T] =
+      for {
+        headJson  <- json(head.name)
+        headValue <- head.schema.codec.getOption(headJson)
+        tailJson = json.remove(head.name)
+        tailValue <- tail.objectCodec.getOption(tailJson)
+      } yield headValue :: tailValue
 
     def reverseGet(value: H :: T): JsonObject = {
       val headValue :: tailValue = value
-      val result = tail.objectCodec.reverseGet(tailValue)
+      val result                 = tail.objectCodec.reverseGet(tailValue)
       result.add(head.name, head.schema.codec.reverseGet(headValue))
     }
 
